bc4ca70c74637199ab7407cc3b502fee8e15620b
rchow <rita.chow@gmail.com>
2016-03-26 17:35:24 -0700

    Set profile image and name from Facebook.

--- Oye5/Source/ViewControllers/OYEProfileViewController.m
+++ Oye5/Source/ViewControllers/OYEProfileViewController.m
@@ -6,6 +6,9 @@
 //  Copyright © 2016 Oye5. All rights reserved.
 //
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <AFNetworking/UIImageView+AFNetworking.h>
+
 #import "OYEProfileViewController.h"
 
 #import "UIColor+Extensions.h"
@@ -32,6 +35,8 @@ static NSString * const cellReuseIdentifier = @"cell";
     [self setupImageButton];
     [self setupNameLabel];
     [self setupTableView];
+    [self setupFacebook];
+    [self setupNotifications];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -58,6 +63,72 @@ static NSString * const cellReuseIdentifier = @"cell";
 
 - (void)setupTableView {
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellReuseIdentifier];
+}
+
+- (void)setupFacebook {
+    [FBSDKProfile enableUpdatesOnAccessTokenChange:YES];
+    
+    [self setupUIWithFacebookProfile];
+}
+
+- (void)setupUIWithFacebookProfile {
+    //    if ([FBSDKAccessToken currentAccessToken]) {
+    //        [self setupProfilePictureFromFacebookWithUserID:[FBSDKAccessToken currentAccessToken].userID];
+    //    }
+    
+    [self resetUI];
+    
+    if ([FBSDKAccessToken currentAccessToken]) {
+        if ([FBSDKProfile currentProfile]) {
+            [self setupProfilePictureFromFacebookWithUserID:[FBSDKProfile currentProfile].userID];
+            
+            self.nameLabel.text = [FBSDKProfile currentProfile].name;
+        } else {
+            // Fields can be found at:  https://developers.facebook.com/docs/graph-api/reference/user
+            NSString *parameterValue = [NSString stringWithFormat:@"id,name,picture.width(%@).height(%@)", @(self.imageView.width), @(self.imageView.height)];
+
+            __weak __typeof(self) weakSelf = self;
+            [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:@{ @"fields" : parameterValue}]startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+                if (!error) {
+                    NSString *nameOfLoginUser = [result valueForKey:@"name"];
+                    
+                    weakSelf.nameLabel.text = nameOfLoginUser;
+                    
+                    NSString *imageStringOfLoginUser = [[[result valueForKey:@"picture"] valueForKey:@"data"] valueForKey:@"url"];
+                    NSURL *url = [[NSURL alloc] initWithString:imageStringOfLoginUser];
+                    
+                    [weakSelf.imageView setImageWithURL:url placeholderImage: nil];
+                 }
+            }];
+        }
+    }
+}
+
+- (void)setupProfilePictureFromFacebookWithUserID:(NSString *)userID {
+    FBSDKProfilePictureView *fbPictureView = [FBSDKProfilePictureView new];
+    fbPictureView.pictureMode = FBSDKProfilePictureModeSquare;
+    fbPictureView.profileID = userID;
+    fbPictureView.frame = self.imageView.bounds;
+    
+    [fbPictureView setNeedsImageUpdate];
+    
+    [self.imageView addSubview:fbPictureView];
+    
+}
+
+- (void)setupNotifications {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(profileDidChangeNotification:) name:FBSDKProfileDidChangeNotification object:nil];
+}
+
+- (void)resetUI {
+    for (UIView *aSubview in self.imageView.subviews) {
+        if ([aSubview isKindOfClass:[FBSDKProfilePictureView class]]) {
+            [aSubview removeFromSuperview];
+        }
+    }
+    
+    self.imageView.image = nil;
+    self.nameLabel.text = nil;
 }
 
 - (void)deleteImage {
@@ -154,6 +225,13 @@ static NSString * const cellReuseIdentifier = @"cell";
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
+}
+
+#pragma mark - Notifications
+
+- (void)profileDidChangeNotification:(NSNotification *)notification {
+    
+    [self setupUIWithFacebookProfile];
 }
 
 /*
